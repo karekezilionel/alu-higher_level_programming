047d3514f55dd9edd381319bdf2c5c0e5c703881
karekezilionel <l.karekezi@alustudent.com>
2025-03-13 05:35:22 -0700

    done

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 
-"""task 7 module"""
+"""Module for adding command-line arguments to a list and saves them to a file"""
 
 import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_file
+def main():
+    """Adds arguments to a list and saves them to a JSON file."""
+    
+    try:
+        items = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        items = []
+    
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, 'add_item.json')
 
-arglist = list(sys.argv[1:])
 
-try:
-    old_data = load_from_json_file("add_item.json")
-except Exception:
-    old_data = []
+if __name__ == '__main__':
+    main()
 
-    old_data.extend(old_data, "add_item.json")
