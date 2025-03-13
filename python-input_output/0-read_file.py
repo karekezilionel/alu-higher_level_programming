796f2ca7749e3c2c46b8428c2a9caa90cebf5bf5
karekezilionel <l.karekezi@alustudent.com>
2025-03-13 04:33:46 -0700

    done

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,8 +2,8 @@
 
 """Module that defines the read_file function."""
 
+
 def read_file(filename=""):
     """Read a UTF-8 encoded file and print its contents."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-
