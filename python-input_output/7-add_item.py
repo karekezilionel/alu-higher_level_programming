3461a1809d2f07888eebf32ddf5dbb5b98bbaa13
karekezilionel <l.karekezi@alustudent.com>
2025-03-13 09:57:41 -0700

    done

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
+""" Load, add, save  """
+from sys import argv
 
-"""Task 7 module"""
 
-import sys
-import json
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-# Import necessary functions
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
-
-# Collect arguments from the command line
-arglist = list(sys.argv[1:])
-
-# Attempt to load existing data from the JSON file
+""" Script that adds all arguments to a Python list,
+and then save them to a file """
 try:
-    old_data = load_from_json_file("add_item.json")
+    add_i = load_from_json_file('add_item.json')
+    save_to_json_file(add_i + argv[1:], 'add_item.json')
 except Exception:
-    old_data = []
-
-# Add new data from the command-line arguments
-old_data.extend(arglist)
-
-# Save updated data back to the JSON file
-save_to_json_file(old_data, "add_item.json")
-
+    save_to_json_file(argv[1:], 'add_item.json')
