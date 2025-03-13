6fc069989c4f883869f6ed0d1676dea997a6910b
karekezilionel <l.karekezi@alustudent.com>
2025-03-13 04:45:33 -0700

    done

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 """defining append_write function"""
 
 
 def append_write(filename="", text=""):
+
     """appends filename with utf-8"""
-    with open(filename, "a" , encoding="utf-8") as f:
+
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
