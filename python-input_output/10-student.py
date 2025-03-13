3cf47aa8367653ef43070e135fca35fc3264e437
karekezilionel <l.karekezi@alustudent.com>
2025-03-13 09:45:35 -0700

    done

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Module"""
+
+
+class Student:
+    """Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Function"""
+        if isinstance(attrs, list) and all(
+            isinstance(attr, str) for attr in attrs
+        ):
+            return {
+                key: getattr(self, key) for key in attrs if hasattr(self, key)
+            }
+        return self.__dict__
