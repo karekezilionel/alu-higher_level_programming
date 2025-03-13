3c18f953b7c515e71dcca39a5fff01dc4cd82105
karekezilionel <l.karekezi@alustudent.com>
2025-03-13 04:51:24 -0700

    done

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""defining to_json_string function"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+
+    """returns json representation of anan object"""
+
+    return json.dumps(my_obj)
