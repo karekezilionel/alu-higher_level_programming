b06717aa5ab1d696d15ddf887420907a8f3f6f93
karekezilionel <l.karekezi@alustudent.com>
2025-03-14 01:52:31 -0700

    done

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Script that deletes all records with a score under or equal to 5
+
+DELETE FROM second_table WHERE score <= 5;
---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Script to calculate the average score
+
+SELECT AVG(score) AS average FROM second_table;
