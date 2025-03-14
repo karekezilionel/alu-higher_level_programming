849351f7c74e22f6c12b8f598b90f2072980e495
karekezilionel <l.karekezi@alustudent.com>
2025-03-14 04:12:14 -0700

    done

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
--- List all cities with their state names
-SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
-ORDER BY cities.id ASC;
+-- List shows with at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
