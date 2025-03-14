a4a2d21bbca1058f036b5ba66880022bcefe19fe
karekezilionel <l.karekezi@alustudent.com>
2025-03-14 03:47:54 -0700

    done

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- SQL
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Create the table states if it does not exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
