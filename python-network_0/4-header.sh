da7122a728c0acdd95c31ce139f8c59ea252582f
karekezilionel <l.karekezi@alustudent.com>
2025-03-22 09:01:10 -0700

    done

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request with the header X-HolbertonSchool-User-Id set to 98 and displays the body of the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
