d11b4d2d38a609ee6ea531f92d74ddee45fd76c8
karekezilionel <l.karekezi@alustudent.com>
2025-03-22 08:32:12 -0700

    done

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A script that takes a URL, sends a GET request, and displays the body of the response only if the status code is 200.
-curl -sL -w "%{http_code}" "$1" -o temp && [ "$(cat temp | tail -c 3)" = "200" ] && head -c -3 temp
+# A script that sends a GET request and displays the body of a 200 status response.
+curl -sL "$1" -o temp && [ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" = "200" ] && cat temp
