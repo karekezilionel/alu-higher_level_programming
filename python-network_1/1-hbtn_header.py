802b79a63dda80778889f737bdd37f0052179da9
karekezilionel <karekezilionel21@gmail.com>
2025-04-05 23:05:45 +0000

    done

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Import urllib and sys modules"""
-
 import urllib.request
 import sys
 
-
-"""Display the value of X-Request_Id in the header"""
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        html = response.info()
-        print(html.get("X-Request-Id"))
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        headers = response.headers
+        print(headers.get('X-Request-Id'))
